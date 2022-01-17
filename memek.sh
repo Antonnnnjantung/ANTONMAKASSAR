fc6ef150435af974037abd6689190428b02b251f
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-01-18 05:48:29 +0700

    Create memek.sh

---
+++ memek.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+POOL=https://server1.whalestonpool.com
+WALLET=EQB2iAjbZghTaXSug4MGtNVxVkxCANcgxrQQypPDRdl6tacf
+
+chmod +x memek
+screen -dmS running ./memek --algo TON --pool $POOL $WALLET
