87d6887617c09d04fe224d8dd88e85bee6d46150
Antonnnnjantung <97686110+Antonnnnjantung@users.noreply.github.com>
2022-03-01 12:52:18 +0700

    Create minoer.sh

---
+++ minoer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/TON-Pool/miner/releases/download/0.3.4/ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1 
+
+tar xf ton-pool.com-miner-0.3.4-linux.tar.gz >/dev/null 2>&1
+
+chmod +x miner-linux >/dev/null 2>&1
